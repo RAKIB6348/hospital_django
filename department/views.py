a146ca8328610dd22c4847eafed05e4e280d53d3
RAKIB6348 <shahidulhuda0@gmail.com>
2026-08-04 23:38:32 +0600

    remove two field inside department model

--- department/views.py
+++ department/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from doctor.models import Doctor
 from .models import Department
 
 
@@ -12,26 +11,20 @@ def department_add(request):
     if request.method == 'POST':
         Department.objects.create(
             name=request.POST['name'],
-            head_doctor=request.POST['head_doctor'],
             number_of_doctors=request.POST['number_of_doctors'],
-            available_beds=request.POST['available_beds'],
         )
         return redirect('department:department_list')
-    doctors = Doctor.objects.all()
-    return render(request, 'department/department_add.html', {'doctors': doctors})
+    return render(request, 'department/department_add.html')
 
 
 def department_edit(request, department_id):
     department = get_object_or_404(Department, id=department_id)
     if request.method == 'POST':
         department.name = request.POST['name']
-        department.head_doctor = request.POST['head_doctor']
         department.number_of_doctors = request.POST['number_of_doctors']
-        department.available_beds = request.POST['available_beds']
         department.save()
         return redirect('department:department_list')
-    doctors = Doctor.objects.all()
-    return render(request, 'department/department_edit.html', {'department': department, 'doctors': doctors})
+    return render(request, 'department/department_edit.html', {'department': department})
 
 
 def department_delete(request, department_id):
